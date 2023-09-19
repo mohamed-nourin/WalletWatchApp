9d51524428cc4e2ade83e0b61618442c326a22c1
Mohamed Nourin <mohamed0nourin@gmail.com>
2023-09-19 15:47:57 +0300

    first commit

---
+++ src/main/java/com/devmountain/WalletWatchApp/WalletWatchApp.java
@@ -0,0 +1,14 @@
+package com.devmountain.WalletWatchApp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WalletWatchApp {
+
+    public static void main(String[] args) {
+        SpringBootApplication.run(WalletWatchApp.class, args);
+
+    }
+
+}
