ed84785c33234cc6c3e64f619096f0520ca1c29c
Mohamed Nourin <mohamed0nourin@gmail.com>
2023-09-27 21:44:12 -0600

    backend finished

--- src/main/java/com/devmountain/WalletWatchApp/dtos/CommentDto.java
+++ src/main/java/com/devmountain/WalletWatchApp/dtos/CommentDto.java
@@ -1,34 +1,34 @@
-package com.devmountain.WalletWatchApp.dtos;
-
-import com.devmountain.WalletWatchApp.entities.Comment;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CommentDto {
-    private Long id;
-    private String comment;
-
-    private UserDto userDto;
-    private ExpenseDto expenseDto;
-
-    public CommentDto(Comment comment){
-        if (comment.getId() != null) {
-            this.id = comment.getId();
-        }
-        if (comment.getComment() != null) {
-            this.comment = comment.getComment();
-        }
-        if (comment.getUser() != null) {
-            this.userDto = new UserDto(comment.getUser());
-        }
-        if (comment.getExpense() != null) {
-            this.expenseDto = new ExpenseDto((comment.getExpense()));
-        }
-
-    }
-
-}
+//package com.devmountain.WalletWatchApp.dtos;
+//
+//import com.devmountain.WalletWatchApp.entities.Comment;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CommentDto {
+//    private Long id;
+//    private String comment;
+//
+//    private UserDto userDto;
+//    private ExpenseDto expenseDto;
+//
+//    public CommentDto(Comment comment){
+//        if (comment.getId() != null) {
+//            this.id = comment.getId();
+//        }
+//        if (comment.getComment() != null) {
+//            this.comment = comment.getComment();
+//        }
+//        if (comment.getUser() != null) {
+//            this.userDto = new UserDto(comment.getUser());
+//        }
+//        if (comment.getExpense() != null) {
+//            this.expenseDto = new ExpenseDto((comment.getExpense()));
+//        }
+//
+//    }
+//
+//}
